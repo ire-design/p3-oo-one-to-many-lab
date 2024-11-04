8a308c16c4bfa4e6ededdf2601a24920b35af03c
Irene Musau <irenemwikalii04@gmail.com>
2024-11-04 10:43:07 +0300

    start

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -3,4 +3,16 @@ class Pet:
     
     all = [] 
     
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        
+        if self.pet_type not in Pet.PET_TYPES:
+            raise Exception(f"Invalid pet type: {self.pet_type}")
+        
+        Pet.all.append(self) 
     
+    def __repr__(self):
+        return f"{self.name} ({self.pet_type})"
+
