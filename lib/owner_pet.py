d6cfeaf5deacf564f15b2b4e6ff327c82ec49ae9
Irene Musau <irenemwikalii04@gmail.com>
2024-11-04 10:43:41 +0300

    done

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -16,3 +16,18 @@ class Pet:
     def __repr__(self):
         return f"{self.name} ({self.pet_type})"
 
+class Owner:
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        
+        if not isinstance(pet, Pet):
+            raise Exception("Only Pet instances can be added.")
+        pet.owner = self  
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
